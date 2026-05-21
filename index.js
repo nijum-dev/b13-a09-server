02b895d4c6a11afbbcb0b00f0275915827c67ede
nijum-dev <jannatnijum.dev@gmail.com>
2026-05-21 11:05:13 +0600

    Created env file and connected with main

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Created Backend server
+
 const express = require('express')
 const dotenv = require('dotenv')
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -19,13 +19,13 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
-    // Send a ping to confirm a successful connection
+    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
+    
     await client.close();
   }
 }
@@ -35,5 +35,5 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.listen(port, () => {
-  console.log(`Running on port ${port}`)
+  console.log(`Smoothly Running on port ${port}`)
 })
